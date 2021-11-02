6de2b6c50f192aae3ea6b1da3e2b1e83e4e83fc9
GMH <rhalsgur3151@gmail.com>
2021-11-02 21:53:19 +0900

    211102 / No.3053 Clear

---
+++ 3053.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    
+    double pi = acos(-1.0);
+    double r, result1, result2;
+
+    scanf("%lf", &r);
+
+    result1 = pow(r,2) * pi;
+    result2 = 2 * pow(r,2);
+
+    printf("%.6lf\n%.6lf", result1, result2);
+
+}
